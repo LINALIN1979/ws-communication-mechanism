02021fac988c1106459e4c2f5836263dc014f865
linalin1979@gmail.com <linalin1979@gmail.com@fc631aef-dbe9-7bd2-65f5-9f18d6c0d3ba>
2012-11-06 02:45:59 +0000

    1. Due to create worker pthread infinitely generate lots of physical/virtual memory usage, use threadpool to do the worker job. 2. Modify client part and make it more clear.

--- c_sample_worker/main.c
+++ c_sample_worker/main.c
@@ -6,15 +6,18 @@
 #include "../wsclient/client.h"
 #include <signal.h>
 
-#define CLIENT 	0
+#define DO_CLIENT 	0
 
-worker_t *worker = NULL;
-#if CLIENT
-client_t *client = NULL;
+worker_t 		*worker = NULL;
+threadpool_t	*threads = NULL;
+#if DO_CLIENT
+client_t 		*client = NULL;
 #endif
 
 // fake update
-void *sleep_then_return(void *ptr)
+//void *
+void
+sleep_then_return(void *ptr)
 {
 	if(worker) {
 		char *taskid = (char *)ptr;
@@ -24,79 +27,13 @@ void *sleep_then_return(void *ptr)
 		}
 		free(taskid);
 	}
-	return NULL;
+	//return NULL;
 }
 
-int oplong(char *taskid, char *method, char *data)
+#if DO_CLIENT
+void
+fake_client_request(void *ptr)
 {
-	pthread_t tmp;
-	if(taskid)	printf("oplong: taskid = %s\n", taskid);
-	if(method)	printf("oplong: method = %s\n", method);
-	if(data)	printf("oplong: data = %s\n", data);
-	char *task = strdup(taskid);
-	pthread_create(&tmp, NULL, &sleep_then_return, (void *)task);
-	return OPLONG_ACCEPT; // or OPLONG_REJECT
-}
-
-char* opshort(char *method, char *data)
-{
-	if(method)	printf("opshort: method = %s\n", method);
-	if(data)	printf("opshort: data = %s\n", data);
-
-	if(!strcmp(data, "what time is it?"))
-	{
-		time_t timer;
-	    static char buffer[25];
-	    struct tm* tm_info;
-	    time(&timer);
-	    tm_info = localtime(&timer);
-
-	    strftime(buffer, 25, "%Y:%m:%d%H:%M:%S", tm_info);
-	    //free(tm_info); <-- tm_info can't be free()
-	    return buffer;
-	}
-	return NULL;
-}
-
-void my_handler(int s)
-{
-	printf("SIGINT received...\n");
-	printf(" Terminate the worker\n");
-	worker_destroy(&worker);
-#if CLIENT
-	printf(" Terminate the client\n");
-	client_destroy(&client);
-#endif
-	exit(0);
-}
-
-int main (int argc, char *argv [])
-{
-	if(argc != 3) {
-		printf("Usage: %s tcp://<dispatcher address>:<port> <your hostname>\n", argv[0]);
-		return 0;
-	}
-
-	worker = worker_create(argv[2], argv[1], &oplong, &opshort);
-	if(!worker) {
-		printf("WTF! why there is no worker created?\n");
-		goto err;
-	}
-#if CLIENT
-	client = client_create(argv[1]);
-	if(!client) {
-		printf("WTF! why there is no client created?\n");
-		goto err;
-	}
-#endif
-
-	struct sigaction sigIntHandler;
-	sigIntHandler.sa_handler = &my_handler;
-	sigemptyset(&sigIntHandler.sa_mask);
-	sigIntHandler.sa_flags = 0;
-	sigaction(SIGINT, &sigIntHandler, NULL);
-
-#if CLIENT
 	zclock_sleep(5000);
 	printf("oplong: ");
 	char *taskid = client_oplong(client, "time.1", "method:time", "what time is it?");
@@ -104,7 +41,7 @@ int main (int argc, char *argv [])
 		int quit = 0;
 		unsigned int percentage;
 		while(!quit) {
-			zclock_sleep(700);
+			zclock_sleep(1000);
 			percentage = client_querytask(client, taskid);
 			switch(percentage)
 			{
@@ -134,6 +71,99 @@ int main (int argc, char *argv [])
 	char *ret = client_opshort(client, "time", "method:time", "what time is it?");
 	if(ret)	printf("%s\n", ret);
 	else	printf("fail\n");
+}
+#endif
+
+int oplong(char *taskid, char *method, char *data)
+{
+	//pthread_t tmp;
+	if(taskid)	printf("oplong: taskid = %s\n", taskid);
+	if(method)	printf("oplong: method = %s\n", method);
+	if(data)	printf("oplong: data = %s\n", data);
+	char *task = strdup(taskid);
+	//pthread_create(&tmp, NULL, &sleep_then_return, (void *)task);
+	if(threads) {
+		if(threadpool_add(threads, &sleep_then_return, (void *)task, 0) != 0) {
+			printf("threadpool_add failed, task ID = %s\n", task);
+			FREE(task);
+		}
+	}
+	return OPLONG_ACCEPT; // or OPLONG_REJECT
+}
+
+char* opshort(char *method, char *data)
+{
+	if(method)	printf("opshort: method = %s\n", method);
+	if(data)	printf("opshort: data = %s\n", data);
+
+	if(!strcmp(data, "what time is it?"))
+	{
+		time_t timer;
+	    static char buffer[25];
+	    struct tm* tm_info;
+	    time(&timer);
+	    tm_info = localtime(&timer);
+
+	    strftime(buffer, 25, "%Y:%m:%d%H:%M:%S", tm_info);
+	    //free(tm_info); <-- tm_info can't be free()
+	    return buffer;
+	}
+	return NULL;
+}
+
+void destroy()
+{
+	if(threads)
+		threadpool_destroy(threads, 0);
+	printf(" Terminate the worker\n");
+	worker_destroy(&worker);
+#if DO_CLIENT
+	printf(" Terminate the client\n");
+	client_destroy(&client);
+#endif
+}
+
+void my_handler(int s)
+{
+	printf("SIGINT received...\n");
+	destroy();
+	exit(0);
+}
+
+int main (int argc, char *argv [])
+{
+	if(argc != 3) {
+		printf("Usage: %s tcp://<dispatcher address>:<port> <your hostname>\n", argv[0]);
+		return 0;
+	}
+
+	threads = threadpool_create(3, 5, 0);
+
+	worker = worker_create(argv[2], argv[1], &oplong, &opshort);
+	if(!worker) {
+		printf("WTF! why there is no worker created?\n");
+		goto err;
+	}
+#if DO_CLIENT
+	client = client_create(argv[1]);
+	if(!client) {
+		printf("WTF! why there is no client created?\n");
+		goto err;
+	}
+#endif
+
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = &my_handler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+
+#if DO_CLIENT
+	if(threads) {
+		if(threadpool_add(threads, &fake_client_request, NULL, 0) != 0) {
+			printf("Create fake_client_request failed\n");
+		}
+	}
 #endif
 
 	while(1) {
@@ -142,10 +172,6 @@ int main (int argc, char *argv [])
 	return 0;
 
 err:
-	worker_destroy(&worker);
-#if CLIENT
-	client_destroy(&client);
-#endif
-
+	destroy();
     return -1;
 }
