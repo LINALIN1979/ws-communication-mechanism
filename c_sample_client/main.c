4427f1e52d4d2b481e6fb41ca0ecb2e1c68575c7
linalin1979@gmail.com <linalin1979@gmail.com@fc631aef-dbe9-7bd2-65f5-9f18d6c0d3ba>
2012-11-08 11:17:28 +0000

    1. Change client task creations into indefinitely loop.

--- c_sample_client/main.c
+++ c_sample_client/main.c
@@ -3,41 +3,16 @@
 
 client_t *session = NULL;
 
-void my_handler(int s)
+void
+fake_client_request(void *ptr)
 {
-	printf("SIGINT received, terminate the worker.\n");
-	client_destroy(&session);
-	exit(0);
-}
-
-int main(int argc, char **argv)
-{
-	if(argc != 2)
-	{
-		printf("Usage: %s tcp://<dispatcher IP>:<port>\n", argv[0]);
-		return 0;
-	}
-
-	session = client_create(argv[1]);
-	if(!session) {
-		printf("WTF! why there is no client created?\n");
-		return -1;
-	}
-	else {
-		struct sigaction sigIntHandler;
-		sigIntHandler.sa_handler = &my_handler;
-		sigemptyset(&sigIntHandler.sa_mask);
-		sigIntHandler.sa_flags = 0;
-		sigaction(SIGINT, &sigIntHandler, NULL);
-	}
-
 	printf("oplong: ");
 	char *taskid = client_oplong(session, "time.1", "method:time", "what time is it?");
 	if(taskid) {
 		int quit = 0;
 		unsigned int percentage;
 		while(!quit) {
-			zclock_sleep(700);
+			zclock_sleep(1000);
 			percentage = client_querytask(session, taskid);
 			switch(percentage)
 			{
@@ -66,10 +41,48 @@ int main(int argc, char **argv)
 
 	printf("opshort: ");
 	char *ret = client_opshort(session, "time", "method:time", "what time is it?");
-	if(ret)	printf("%s\n", ret);
-	else	printf("fail\n");
-	free(ret);
+	if(ret)	{ printf("%s\n", ret); free(ret); }
+	else	{ printf("fail\n"); }
+}
 
+void destroy()
+{
 	client_destroy(&session);
+	printf("Terminated...\n");
+}
+
+void my_handler(int s)
+{
+	printf("SIGINT received, terminate the client.\n");
+	destroy();
+	exit(0);
+}
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		printf("Usage: %s tcp://<dispatcher IP>:<port>\n", argv[0]);
+		return 0;
+	}
+
+	session = client_create(argv[1]);
+	if(!session) {
+		printf("WTF! why there is no client created?\n");
+		return -1;
+	}
+
+	struct sigaction sigIntHandler;
+	sigIntHandler.sa_handler = &my_handler;
+	sigemptyset(&sigIntHandler.sa_mask);
+	sigIntHandler.sa_flags = 0;
+	sigaction(SIGINT, &sigIntHandler, NULL);
+
+	while(1) {
+		fake_client_request(session);
+		zclock_sleep(1000);
+	}
+
+	destroy();
 	return 0;
 }
