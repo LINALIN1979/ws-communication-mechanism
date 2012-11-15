0b14565127c94176108fa110e4451c6fce7089db
linalin1979@gmail.com <linalin1979@gmail.com@fc631aef-dbe9-7bd2-65f5-9f18d6c0d3ba>
2012-11-15 03:17:37 +0000

    1. feature: add task_create_manually, which could create task_t with customized values. 2. feature: add task_set_client_str, task_set_worker_str. 3. feature: add task_get_createTime. 4. feature: change task_get_clientstr to task_get_client_str, task_get_workerstr to task_get_worker_str, task_get_servicename to task_get_serviceName.

--- wsdispatcher/task.h
+++ wsdispatcher/task.h
@@ -12,6 +12,10 @@
 typedef struct _task_t task_t;
 
 task_t*	task_create(char *service_name, zframe_t *client, char *method, char *data, zframe_t *worker);
+task_t*	task_create_manually(char *taskID, unsigned int status, int dispatched,
+			uint64_t createTime, uint64_t timeout_old, uint64_t timeout_new, uint64_t timeout_interval,
+			char *serviceName, char *method, char *data,
+			char *client_str, char *worker_str);
 void	task_destroy(void *argument);
 
 int		task_get_status(task_t *self);
@@ -21,16 +25,19 @@ int		task_get_dispatched(task_t *self);
 void	task_set_dispatched(task_t *self, int dispatched);
 
 zframe_t*	task_get_client(task_t *self);
-char*		task_get_clientstr(task_t *self);
+char*		task_get_client_str(task_t *self);
 void		task_set_client(task_t *self, zframe_t *client);
+void		task_set_client_str(task_t *self, char *client_str);
 
 zframe_t*	task_get_worker(task_t *self);
-char*		task_get_workerstr(task_t *self);
+char*		task_get_worker_str(task_t *self);
 void		task_set_worker(task_t *self, zframe_t *worker);
+void		task_set_worker_str(task_t *self, char *worker_str);
 
 char*	task_get_taskID(task_t *self);
+uint64_t	task_get_createTime(task_t *self);
 timeout_t*	task_get_timeout(task_t *self);
-char*	task_get_servicename(task_t *self);
+char*	task_get_serviceName(task_t *self);
 char*	task_get_method(task_t *self);
 char*	task_get_data(task_t *self);
 int		task_get_expired(task_t *self);
